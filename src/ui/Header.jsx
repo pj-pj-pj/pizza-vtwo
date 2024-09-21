06e556b28bbb659c8b52a675e4d786f75a6142af
Paula Joyce Ucol <peaksenut@gmail.com>
2024-09-22 00:50:10 +0800

    set up layout route

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Good Pizza, Bad Pizza</Link>
+    </header>
+  );
+}
+
+export default Header;
