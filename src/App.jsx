a52ac8f0c646e1fd504036c19564de3a9dbdc035
Paula Joyce Ucol <peaksenut@gmail.com>
2024-09-22 13:55:57 +0800

    add visual loader when loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
