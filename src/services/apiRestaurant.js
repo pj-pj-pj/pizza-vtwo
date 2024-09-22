e2aad90cdadaecd823b121f896a092b9f8d4a00f
Paula Joyce Ucol <peaksenut@gmail.com>
2024-09-22 13:36:18 +0800

    fake pizza data api fixed

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,17 +1,19 @@
-const API_URL = 'https://react-fast-pizza-api.onrender.com/api';
+// const API_URL = "https://react-fast-pizza-api.onrender.com/api";
+const BASE_URL = "http://localhost:9000";
 
 export async function getMenu() {
-  const res = await fetch(`${API_URL}/menu`);
+  // const res = await fetch(`${API_URL}/menu`);
+  const res = await fetch(`${BASE_URL}/menu/`);
 
   // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
-  if (!res.ok) throw Error('Failed getting menu');
+  if (!res.ok) throw Error("Failed getting menu");
 
   const { data } = await res.json();
   return data;
 }
 
 export async function getOrder(id) {
-  const res = await fetch(`${API_URL}/order/${id}`);
+  const res = await fetch(`${BASE_URL}/order/${id}`);
   if (!res.ok) throw Error(`Couldn't find order #${id}`);
 
   const { data } = await res.json();
@@ -20,11 +22,11 @@ export async function getOrder(id) {
 
 export async function createOrder(newOrder) {
   try {
-    const res = await fetch(`${API_URL}/order`, {
-      method: 'POST',
+    const res = await fetch(`${BASE_URL}/order`, {
+      method: "POST",
       body: JSON.stringify(newOrder),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
@@ -32,23 +34,23 @@ export async function createOrder(newOrder) {
     const { data } = await res.json();
     return data;
   } catch {
-    throw Error('Failed creating your order');
+    throw Error("Failed creating your order");
   }
 }
 
 export async function updateOrder(id, updateObj) {
   try {
-    const res = await fetch(`${API_URL}/order/${id}`, {
-      method: 'PATCH',
+    const res = await fetch(`${BASE_URL}/order/${id}`, {
+      method: "PATCH",
       body: JSON.stringify(updateObj),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (!res.ok) throw Error();
     // We don't need the data, so we don't return anything
   } catch (err) {
-    throw Error('Failed updating your order');
+    throw Error("Failed updating your order");
   }
 }
